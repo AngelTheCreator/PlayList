b9c6439fea63541285c0e58ee86aa7b1f9666dc9
AngeloAbat <angeloabat25@gmail.com>
2022-11-10 19:37:53 -0800

    Routes added, prototypes still needs tinkering

---
+++ src/components/Prototypes/displaydata.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function DisplayProto(){
+    const[data, setData] = useState([])
+
+
+    useEffect(() => {
+        const SearchEndPoint = 'https://api.igdb.com/v4/games'
+        const fetchData = async() => {
+            const response = await fetch(SearchEndPoint, {
+                method: 'POST',
+                mode: 'no-cors',
+                headers:{
+                    "Access-Control-Allow-Origin": '*',
+                    "Access-Control-Allow-Methods": '*',
+                    "client_id" : "KEYHERE",
+                    "authorization" : 'Bearer KEYHERE',
+                    'Content-type': 'application/json',
+                }
+            })
+            console.log("I am here:  " + response)
+            const resData = await response.json()
+            setData(resData)
+        }
+        fetchData()
+    }, [])
+    
+    console.log(data)
+
+    return(
+        <div>
+
+        </div>
+    )
+}
